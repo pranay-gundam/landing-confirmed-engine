dc7e8835ea404536c0f496816d3c67ddc7103029
pranay-gundam <pranaygundam00@gmail.com>
2024-10-05 22:02:56 -0400

    some basic shapes

--- src/game_objects.py
+++ src/game_objects.py
@@ -1,12 +1,28 @@
 from typing import Tuple, List
+import numpy as np
+import shapely as sp
 
 class GameObject:
-    def __init__(self, border: List[Tuple[int, int]]):
-        self.border = border
+    def __init__(self, border: List[Tuple[float, float]], durability: float = np.inf):
+        self.shape = sp.geometry.Polygon(border)
+        self.durability = durability
 
+    def is_collided(self, other_obj):
+        pass
 
-    def move(self, dx, dy):
+    def draw(self):
+        raise NotImplementedError
+    
+
+class MoveableGameObject(GameObject):
+    def __init__(self, border: List[Tuple[float, float]], durability: float = np.inf):
+        super().__init__(border, durability=durability)        
+
+    def move_rectangular(self, dx, dy):
+        self.shape = sp.affinity.translate(self.shape, xoff=dx, yoff=dy)
         
+    def move_polar(self, dr, dtheta):
+        self.shape = sp.affinity.translate(self.shape, xoff=dr * np.cos(dtheta), yoff= dr * np.sin(dtheta))
 
     def draw(self):
         raise NotImplementedError
